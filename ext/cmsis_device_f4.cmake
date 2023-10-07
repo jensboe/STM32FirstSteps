fff848f5f658ed02cc24987ecc37fca937a8d576
Jens Böckmann <jensboe@outlook.de>
2023-10-07 18:04:10 +0200

    Use startup file from cmsis_device_f4

--- ext/cmsis_device_f4.cmake
+++ ext/cmsis_device_f4.cmake
@@ -1,4 +1,6 @@
-add_library(cmsis_device_f4 INTERFACE)
+add_library(cmsis_device_f4 STATIC
+    cmsis_device_f4/Source/Templates/gcc/startup_stm32f446xx.s
+)
 
 target_include_directories(
     cmsis_device_f4 INTERFACE
