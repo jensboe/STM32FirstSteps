ccd36ab2f636440d9b6568f78ecb5d1dcca04172
Jens Böckmann <jensboe@outlook.de>
2023-10-08 14:56:08 +0200

    Add PLL structure

--- lib/cpphalf4/inc/Rcc.hpp
+++ lib/cpphalf4/inc/Rcc.hpp
@@ -11,20 +11,6 @@ struct Rcc
 		PLLR = RCC_CFGR_SW_PLLR,
 	};
 
-	enum class PLLSource : uint32_t
-	{
-		HSI = RCC_PLLCFGR_PLLSRC_HSI,
-		HSE = RCC_PLLCFGR_PLLSRC_HSE,
-	};
-
-	enum class PLLP : uint32_t
-	{
-		DIV2 = 0b00,
-		DIV4 = 0b01,
-		DIV6 = 0b10,
-		DIV8 = 0b11,
-	};
-
 	static inline void SetSystemClockSource(SystemClockSource source = SystemClockSource::PLLCLK, uint32_t timeout = 2000)
 	{
 		// Set the System clock Switch
@@ -45,90 +31,109 @@ struct Rcc
 		return SystemClockSource((RCC->CFGR & RCC_CFGR_SWS) >> (RCC_CFGR_SWS_Pos - RCC_CFGR_SW_Pos));
 	}
 
-	static inline void enablePLL(uint32_t timeout = 2000)
+	struct PLL
 	{
-		RCC->CR |= RCC_CR_PLLON;
-		while (not PLLready())
+		enum class Source : uint32_t
 		{
-			if (--timeout < 0)
+			HSI = RCC_PLLCFGR_PLLSRC_HSI,
+			HSE = RCC_PLLCFGR_PLLSRC_HSE,
+		};
+
+		enum class P : uint32_t
+		{
+			DIV2 = 0b00,
+			DIV4 = 0b01,
+			DIV6 = 0b10,
+			DIV8 = 0b11,
+		};
+
+		static inline bool isReady()
+		{
+			return RCC->CR & RCC_CR_PLLRDY;
+		}
+
+		static inline void enable(uint32_t timeout = 2000)
+		{
+			RCC->CR |= RCC_CR_PLLON;
+			while (not isReady())
 			{
-				return;
+				if (--timeout < 0)
+				{
+					return;
+				}
 			}
 		}
-	}
 
-	static inline void disablePLL(uint32_t timeout = 2000)
-	{
-		RCC->CR &= ~RCC_CR_PLLON;
-		while (PLLready())
+		static inline void disable(uint32_t timeout = 2000)
 		{
-			if (--timeout < 0)
+			RCC->CR &= ~RCC_CR_PLLON;
+			while (isReady())
 			{
-				return;
+				if (--timeout < 0)
+				{
+					return;
+				}
 			}
 		}
-	}
 
-	static inline bool PLLready()
-	{
-		return RCC->CR & RCC_CR_PLLRDY;
-	}
+		static inline Source GetSource()
+		{
+			return Source(RCC->PLLCFGR & RCC_PLLCFGR_PLLSRC);
+		}
 
-	static inline void SetPLLSource(PLLSource source = PLLSource::HSE, uint32_t timeout = 2000)
-	{
-		RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLSRC) | uint32_t(source);
-	}
-	static inline PLLSource GetPLLSource()
-	{
-		return PLLSource(RCC->PLLCFGR & RCC_PLLCFGR_PLLSRC);
-	}
+		static inline size_t GetM()
+		{
+			return (RCC->PLLCFGR & RCC_PLLCFGR_PLLM) >> RCC_PLLCFGR_PLLM_Pos;
+		}
 
-	static inline void SetPLLM(const size_t value = 4)
-	{
-		RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLM) | (value << RCC_PLLCFGR_PLLM_Pos);
-	}
-	static inline size_t GetPLLM()
-	{
-		return (RCC->PLLCFGR & RCC_PLLCFGR_PLLM) >> RCC_PLLCFGR_PLLM_Pos;
-	}
+		static inline size_t GetN()
+		{
+			return (RCC->PLLCFGR & RCC_PLLCFGR_PLLN) >> RCC_PLLCFGR_PLLN_Pos;
+		}
 
-	static inline void SetPLLN(const size_t value = 168)
-	{
-		RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLN) | (value << RCC_PLLCFGR_PLLN_Pos);
-	}
+		static inline P GetP()
+		{
+			return P((RCC->PLLCFGR & RCC_PLLCFGR_PLLP) >> RCC_PLLCFGR_PLLP_Pos);
+		}
 
-	static inline size_t GetPLLN()
-	{
-		return (RCC->PLLCFGR & RCC_PLLCFGR_PLLN) >> RCC_PLLCFGR_PLLN_Pos;
-	}
+		static inline size_t GetQ()
+		{
+			return (RCC->PLLCFGR & RCC_PLLCFGR_PLLQ) >> RCC_PLLCFGR_PLLQ_Pos;
+		}
 
-	static inline void SetPLLP(const PLLP value = PLLP::DIV2)
-	{
-		RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLP) | (uint32_t(value) << RCC_PLLCFGR_PLLP_Pos);
-	}
+		static inline size_t GetR()
+		{
+			return (RCC->PLLCFGR & RCC_PLLCFGR_PLLR) >> RCC_PLLCFGR_PLLR_Pos;
+		}
 
-	static inline PLLP GetPLLP()
-	{
-		return PLLP((RCC->PLLCFGR & RCC_PLLCFGR_PLLP) >> RCC_PLLCFGR_PLLP_Pos);
-	}
+		static inline void SetSource(Source source = Source::HSE, uint32_t timeout = 2000)
+		{
+			RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLSRC) | uint32_t(source);
+		}
 
-	static inline void SetPLLQ(const size_t value = 7)
-	{
-		RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLQ) | (value << RCC_PLLCFGR_PLLQ_Pos);
-	}
+		static inline void SetM(const size_t value = 4)
+		{
+			RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLM) | (value << RCC_PLLCFGR_PLLM_Pos);
+		}
 
-	static inline size_t GetPLLQ()
-	{
-		return (RCC->PLLCFGR & RCC_PLLCFGR_PLLQ) >> RCC_PLLCFGR_PLLQ_Pos;
-	}
+		static inline void SetN(const size_t value = 168)
+		{
+			RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLN) | (value << RCC_PLLCFGR_PLLN_Pos);
+		}
 
-	static inline void SetPLLR(const size_t value = 2)
-	{
-		RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLR) | (value << RCC_PLLCFGR_PLLR_Pos);
-	}
+		static inline void SetP(const P value = P::DIV2)
+		{
+			RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLP) | (uint32_t(value) << RCC_PLLCFGR_PLLP_Pos);
+		}
 
-	static inline size_t GetPLLR()
-	{
-		return (RCC->PLLCFGR & RCC_PLLCFGR_PLLR) >> RCC_PLLCFGR_PLLR_Pos;
-	}
+		static inline void SetQ(const size_t value = 7)
+		{
+			RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLQ) | (value << RCC_PLLCFGR_PLLQ_Pos);
+		}
+
+		static inline void SetR(const size_t value = 2)
+		{
+			RCC->PLLCFGR = (RCC->PLLCFGR & ~RCC_PLLCFGR_PLLR) | (value << RCC_PLLCFGR_PLLR_Pos);
+		}
+	};
 };
