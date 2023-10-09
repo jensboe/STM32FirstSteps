e682d42ff08e21eca1a6c7ec6a65dbafce04cf95
Jens Böckmann <jensboe@outlook.de>
2023-10-09 20:27:14 +0200

    Nvic: Add setPriorityGrouping

---
+++ lib/cpphalf4/inc/Nvic.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#pragma once
+#include <cstdint>
+
+#include "stm32f446xx.h"
+struct Nvic {
+    enum class Grouping
+    {
+        PG0 = 0b111,
+        PG1 = 0b110,
+        PG2 = 0b101,
+        PG3 = 0b100,
+        PG4 = 0b011,
+
+    };
+    static inline void setPriorityGrouping(const Grouping grouping)
+    {
+    	NVIC_SetPriorityGrouping(uint32_t(grouping));
+    }
+};
