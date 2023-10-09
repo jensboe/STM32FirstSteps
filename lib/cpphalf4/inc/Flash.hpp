4d304320c802981c83ee484c8fb1e987d44f71e9
Jens Böckmann <jensboe@outlook.de>
2023-10-09 20:26:53 +0200

    Flash: Add cache enabling

--- lib/cpphalf4/inc/Flash.hpp
+++ lib/cpphalf4/inc/Flash.hpp
@@ -4,8 +4,43 @@
 #include "stm32f446xx.h"
 struct Flash
 {
-    static inline void setLatency(uint32_t value)
+    static inline void setLatency(const uint32_t value)
     {
         FLASH->ACR = (FLASH->ACR & ~FLASH_ACR_LATENCY) | ((value &0xF) << FLASH_ACR_LATENCY_Pos);
+    }
+    static inline void setICEN(const bool value)
+    {
+        if(value)
+        {
+            FLASH->ACR |= FLASH_ACR_ICEN;
+        }
+        else
+        {
+            FLASH->ACR &= ~FLASH_ACR_ICEN;
+        }
+    }
+
+    static inline void setDCEN(const bool value)
+    {
+        if(value)
+        {
+            FLASH->ACR |= FLASH_ACR_DCEN;
+        }
+        else
+        {
+            FLASH->ACR &= ~FLASH_ACR_DCEN;
+        }
+    }
+
+    static inline void setPRFTEN(const bool value)
+    {
+        if(value)
+        {
+            FLASH->ACR |= FLASH_ACR_PRFTEN;
+        }
+        else
+        {
+            FLASH->ACR &= ~FLASH_ACR_PRFTEN;
+        }
     }
 };
