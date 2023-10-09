502a255698f2d815672952cc634b824cd0e33cce
Jens Böckmann <jensboe@outlook.de>
2023-10-09 22:42:27 +0200

    Make an enum for all peripherals of STM32F446

--- lib/cpphalf4/inc/peripherals.hpp
+++ lib/cpphalf4/inc/peripherals.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "stm32f446.hpp"
 #include "Flash.hpp"
 #include "Nvic.hpp"
 #include "Pwr.hpp"
---
+++ lib/cpphalf4/inc/stm32f446.hpp
@@ -0,0 +1,15 @@
+namespace stm32f446
+{
+    enum class peripherals
+    {
+        usart1,
+        usart2,
+        usart3,
+        uart4,
+        uart5,
+        usart6,
+    };
+}
+namespace stm32f4 = stm32f446;
+
+namespace stm32 = stm32f4;
