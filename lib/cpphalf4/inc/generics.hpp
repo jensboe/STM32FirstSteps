cc320832383de9a0c7034a10e8415c2e06322a9d
Jens Böckmann <jensboe@outlook.de>
2023-10-27 01:06:50 +0200

    Remove redundant functions and remove unused constants

--- lib/cpphalf4/inc/generics.hpp
+++ lib/cpphalf4/inc/generics.hpp
@@ -3,14 +3,8 @@
 
 using namespace SI::literals;
 
-struct generic_clocktree
-{
-    constexpr static uint32_t setting{};
-};
 struct generic_controller
 {
-    constexpr static SI::hertz_t<uint32_t> HSE{8_MHz};
-    constexpr static generic_clocktree clocktree{};
     static void init(void)
     {
     }
