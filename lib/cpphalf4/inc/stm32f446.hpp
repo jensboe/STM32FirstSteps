9ecc91bd506581bfdcc5a14d62ee0e00e0830556
Jens Böckmann <jensboe@outlook.de>
2023-10-27 22:12:08 +0200

    Add constexpr clocktree

--- lib/cpphalf4/inc/stm32f446.hpp
+++ lib/cpphalf4/inc/stm32f446.hpp
@@ -27,9 +27,114 @@ struct stm32f446 : public stm32f4
         uart5,
         usart6,
     };
+    static constexpr SI::hertz_t<uint32_t> HSI = 16_MHz;
+    
+    template<SI::hertz_t<uint32_t> HSE, SI::hertz_t<uint32_t> target_SYSCLK>
+    struct Clocktree
+    {
+        Rcc::SystemClock::Source SysClkMux;
+        Rcc::PLL::config pllcfg;
+        Rcc::AHB::DIV ahb;
+        Rcc::AHB1::DIV ahb1;
+        Rcc::AHB2::DIV ahb2;
 
+        static constexpr Clocktree calculate()
+        {
+            constexpr Clocktree cfg = {
+                .SysClkMux = Rcc::SystemClock::Source::PLLCLK,
+                .pllcfg = {
+                    .src = Rcc::PLL::Source::HSE,
+                    .M = 4,
+                    .N = target_SYSCLK / (HSE / (2 * 4)),
+                    .Pval = Rcc::PLL::P::DIV2,
+                    .Q = 7,
+                    .R = 2,
+                },
+                .ahb = Rcc::AHB::DIV::DIV1,
+                .ahb1 = Rcc::AHB1::DIV::DIV4,
+                .ahb2 = Rcc::AHB2::DIV::DIV2,
+            };
+            return cfg;
+        }
+
+        static constexpr SI::hertz_t<uint32_t> getHSI(void)
+        {
+            return HSI;
+        }
+
+        static constexpr SI::hertz_t<uint32_t> getHSE(void)
+        {
+            return HSE;
+        }
+
+        static constexpr SI::hertz_t<uint32_t> getPLLSourceMux(void)
+        {
+            constexpr auto cfg = calculate();
+            if constexpr (cfg.pllcfg.src == Rcc::PLL::Source::HSE)
+            {
+                return getHSE();
+            }
+            if constexpr (cfg.pllcfg.src == Rcc::PLL::Source::HSI)
+            {
+                return getHSI();
+            }
+        }
+
+        static constexpr SI::hertz_t<uint32_t> getPLLCLK(void)
+        {
+            constexpr auto cfg = calculate();
+            return getPLLSourceMux() / (2 * cfg.pllcfg.M) * cfg.pllcfg.N;
+        }
+
+        static constexpr SI::hertz_t<uint32_t> getPLLR(void)
+        {
+            constexpr auto cfg = calculate();
+            return getPLLSourceMux() / (cfg.pllcfg.R * cfg.pllcfg.M) * cfg.pllcfg.N;
+        }
+
+        static constexpr SI::hertz_t<uint32_t> getSYSCLK(void)
+        {
+            constexpr auto cfg = calculate();
+            if constexpr (cfg.SysClkMux == Rcc::SystemClock::Source::HSI)
+            {
+                return getHSI();
+            }
+            if constexpr (cfg.SysClkMux == Rcc::SystemClock::Source::HSE)
+            {
+                return getHSE();
+            }
+            if constexpr (cfg.SysClkMux == Rcc::SystemClock::Source::PLLCLK)
+            {
+                return getPLLCLK();
+            }
+            if constexpr (cfg.SysClkMux == Rcc::SystemClock::Source::PLLR)
+            {
+                return getPLLR();
+            }
+        }
+        static constexpr SI::hertz_t<uint32_t> getHCLK(void)
+        {
+            constexpr auto cfg = calculate();
+            return getSYSCLK() / cfg.ahb;
+        }
+
+        static constexpr SI::hertz_t<uint32_t> getPCLK1(void)
+        {
+            constexpr auto cfg = calculate();
+            return getHCLK() / cfg.ahb1;
+        }
+        static constexpr SI::hertz_t<uint32_t> getPCLK2(void)
+        {
+            constexpr auto cfg = calculate();
+            return getHCLK() / cfg.ahb2;
+        }
+    };
+
+    template<SI::hertz_t<uint32_t> HSE, SI::hertz_t<uint32_t> target_SYSCLK>
     static void init(void)
     {
+        using clocktree = Clocktree<HSE, target_SYSCLK>;
+        constexpr auto ct_cfg = clocktree::calculate();
         stm32f4::init();
         Flash::setICEN(true);
         Flash::setDCEN(true);
@@ -37,28 +142,18 @@ struct stm32f446 : public stm32f4
         Rcc::APB1::setPWR(true);
         Pwr::setVOS(Pwr::VOS::SCALE1);
         Flash::setLatency(5);
+
         Rcc::HSI::enable();
         Rcc::SystemClock::setSource(Rcc::SystemClock::Source::HSI);
         Rcc::HSE::enable(Rcc::HSE::Type::CLOCK);
-        const Rcc::PLL::config cfg =
-            {
-                .src = Rcc::PLL::Source::HSE,
-                .M = 4,
-                .N = 168,
-                .Pval = Rcc::PLL::P::DIV2,
-                .Q = 7,
-                .R = 2,
-            };
-        Rcc::PLL::set(cfg);
+        Rcc::PLL::set(ct_cfg.pllcfg);
         Rcc::AHB::set(Rcc::AHB::DIV::DIV1);
         Rcc::AHB1::set(Rcc::AHB1::DIV::DIV4);
         Rcc::AHB2::set(Rcc::AHB2::DIV::DIV2);
-        Rcc::PLL48CLK::set(Rcc::PLL48CLK::Source::PLLQ);
-        Rcc::SystemClock::setSource(Rcc::SystemClock::Source::PLLCLK);
-
-        updateSystemCoreClock();
+        Rcc::SystemClock::setSource(ct_cfg.SysClkMux);
+        SystemCoreClock = clocktree::getSYSCLK().value();
     }
+
     static void delay(SI::milli_seconds_t<uint32_t> delay);
-    static void updateSystemCoreClock();
 };
 extern "C" void SysTick_Handler(void);
--- lib/cpphalf4/src/stm32f446.cpp
+++ lib/cpphalf4/src/stm32f446.cpp
@@ -8,65 +8,12 @@ volatile uint32_t myTick;
 
 void stm32f446::delay(SI::milli_seconds_t<uint32_t> delay)
 {
-  uint32_t tickstart = myTick;
-  uint32_t wait = delay.value();
+	uint32_t tickstart = myTick;
+	uint32_t wait = delay.value();
 
-  while((myTick - tickstart) < wait)
-  {
-  }
-}
-
-void stm32f446::updateSystemCoreClock()
-{
-	SI::hertz_t<uint32_t> clk = 16_MHz;
-	SI::hertz_t<uint32_t> HSE = 8_MHz;
-
-	switch (Rcc::SystemClock::getSource())
+	while ((myTick - tickstart) < wait)
 	{
-	case Rcc::SystemClock::Source::HSI:
-		clk = 16_MHz;
-		break;
-	case Rcc::SystemClock::Source::HSE:
-		clk = HSE;
-		break;
-	case Rcc::SystemClock::Source::PLLCLK:
-	case Rcc::SystemClock::Source::PLLR:
-		switch (Rcc::PLL::getSource())
-		{
-		case Rcc::PLL::Source::HSI:
-			clk = 16_MHz;
-			break;
-		case Rcc::PLL::Source::HSE:
-			clk = HSE;
-			break;
-		}
-		clk = (clk / Rcc::PLL::getM()) * Rcc::PLL::getN();
-		break;
 	}
-	switch (Rcc::SystemClock::getSource())
-	{
-	case Rcc::SystemClock::Source::PLLCLK:
-		switch (Rcc::PLL::getP())
-		{
-		case Rcc::PLL::P::DIV2:
-			clk = clk / 2;
-			break;
-		case Rcc::PLL::P::DIV4:
-			clk = clk / 4;
-			break;
-		case Rcc::PLL::P::DIV6:
-			clk = clk / 6;
-			break;
-		case Rcc::PLL::P::DIV8:
-			clk = clk / 8;
-			break;
-		}
-		break;
-	case Rcc::SystemClock::Source::PLLR:
-		clk = clk / Rcc::PLL::getR();
-		break;
-	}
-	SystemCoreClock = clk.value();
 }
 
 extern "C" void SysTick_Handler(void)
