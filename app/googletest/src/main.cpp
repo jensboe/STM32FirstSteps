08327e610e066c1b2f7124e16013e1d197fab1fd
Jens Böckmann <jensboe@outlook.de>
2023-11-18 13:47:58 +0100

    Add test proof of concept

---
+++ app/googletest/src/main.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+TEST(HelloTest, BasicAssertions)
+{
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(HelloTest, Fail)
+{
+    EXPECT_EQ(7 * 6, 43);
+}
